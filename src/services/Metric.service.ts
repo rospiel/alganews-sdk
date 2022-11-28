c300b6517c1062a26735be0d6df277374dc70281
rospiel <rospielberg@gmail.com>
2022-11-28 11:34:06 -0300

    fix.: fixing url service

--- src/services/Metric.service.ts
+++ src/services/Metric.service.ts
@@ -2,33 +2,42 @@ import { Metric } from "../@types";
 import Service from "../Service";
 
 class MetricService extends Service {
-  static readonly REQUEST_MAPPING: string = "/metrics/editor";
-
+  static readonly REQUEST_MAPPING: string =
+    "/metrics/editor";
 
   static getTop3Tags() {
-    return this.Http
-    .get<Metric.EditorTagRatio>(`${this.REQUEST_MAPPING}/top3-tags`)
-    .then(this.getData)
+    return this.Http.get<Metric.EditorTagRatio>(
+      `${this.REQUEST_MAPPING}/top3-tags`
+    ).then(this.getData);
   }
 
   static getEditorMonthlyEarnings() {
-    return this.Http
-    .get<Metric.EditorMonthlyEarnings>(`${this.REQUEST_MAPPING}/monthly-earnings`)
-    .then(this.getData);
+    return this.Http.get<Metric.EditorMonthlyEarnings>(
+      `${this.REQUEST_MAPPING}/monthly-earnings`
+    ).then(this.getData);
   }
 
   static getMonthlyRevenuesExpenses() {
-    return this.Http
-      .get<Metric.MonthlyRevenuesExpenses>(this.REQUEST_MAPPING.concat('monthly-revenues-expenses'), 
-        { headers: {'Content-Type': 'application/json'} })
-      .then(this.getData);
+    return this.Http.get<Metric.MonthlyRevenuesExpenses>(
+      this.REQUEST_MAPPING.concat(
+        "/monthly-revenues-expenses"
+      ),
+      { headers: { "Content-Type": "application/json" } }
+    ).then(this.getData);
   }
 
   static getMonthlyRevenuesExpensesChartJs() {
-    return this.Http
-      .get<Metric.MonthlyRevenuesExpensesChartJs>(this.REQUEST_MAPPING.concat('monthly-revenues-expenses'), 
-        { headers: {'Content-Type': 'application/vnd.alganews.chartjs+json'} })
-      .then(this.getData);
+    return this.Http.get<Metric.MonthlyRevenuesExpensesChartJs>(
+      this.REQUEST_MAPPING.concat(
+        "/monthly-revenues-expenses"
+      ),
+      {
+        headers: {
+          "Content-Type":
+            "application/vnd.alganews.chartjs+json",
+        },
+      }
+    ).then(this.getData);
   }
 }
 
